fc6d194c6035931e2f8d9ce9e7fc0393dd823803
Evan Sosenko <evan@getseam.com>
2023-10-11 21:01:22 +0000

    Fix ruby

--- snippet-playground/ruby/main.rb
+++ snippet-playground/ruby/main.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-require 'seam'
+require 'seamapi'
 
 random_number = rand(1e6).to_i
 api_url = "https://pws#{random_number}#{random_number}.fakeseamconnect.seam.vc"
 
-client = Seam::Client.new(api_url: api_url, api_key: 'seam_apikey1_token')
+client = Seam::Client.new(base_uri: api_url, api_key: 'seam_apikey1_token')
 
 print client.devices.list
