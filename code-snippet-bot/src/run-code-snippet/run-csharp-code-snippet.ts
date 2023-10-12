41b1df03890ca66344bac7eb3793a4eede05c5d1
Andrii <mastafit1@gmail.com>
2023-10-12 20:34:04 +0200

    Docker is running

--- code-snippet-bot/src/run-code-snippet/run-csharp-code-snippet.ts
+++ code-snippet-bot/src/run-code-snippet/run-csharp-code-snippet.ts
@@ -1,38 +1,39 @@
-import * as Fake from "@seamapi/fake-seam-connect"
-import { runInsideContainer } from "./run-inside-container"
+import * as Fake from "@seamapi/fake-seam-connect";
+import { runInsideContainer } from "./run-inside-container";
 
 export const runCsharpCodeSample = async (csharp_sample: string) => {
-  const fake = await Fake.createFake()
-  const seed = await fake.seed()
-  const server = await fake.startServer()
-  
-  // Code to replace the server URL and API key in the C# sample code
+  const fake = await Fake.createFake();
+  const seed = await fake.seed();
+  const server = await fake.startServer();
+
   const modified_csharp_sample = csharp_sample
     .replace(/YourServerUrl/g, server.serverUrl)
-    .replace(/YourApiKey/g, seed.seam_apikey1_token)
-  
+    .replace(/YourApiKey/g, seed.seam_apikey1_token);
+
   const run_sh = `
     echo "${modified_csharp_sample}" > Program.cs
     dotnet run
-  `.trim()
+  `.trim();
 
-  // Running C# code inside a Docker container
   const { stdout, stderr } = await runInsideContainer({
-    command: "/bin/sh",
+    command: "/root/run.sh",
     imageName: "mcr.microsoft.com/dotnet/sdk:6.0",
     filesystem: {
       "/root/run.sh": run_sh,
     },
-  })
+  });
 
-  // Handle and log any errors
+  const logged_content: string[] = [stdout];
+
   if (stderr) {
-    console.error("Error during C# code execution:", stderr)
-    return { execution_result: null, logged_content: [`Error: ${stderr}`] }
+    console.error("Error during C# code execution:", stderr);
+    logged_content.push(`Error: ${stderr}`);
   }
+  console.log(stderr ? null : stdout,'execution_results')
+  console.log(logged_content,'logged_content')
   
   return {
-    execution_result: stdout,
-    logged_content: [stdout]
-  }
-}
+    execution_result: stderr ? null : stdout,
+    logged_content
+  };
+};
--- code-snippet-bot/tests/run-code-snippet/run-csharp-code-snippet-01.test.ts
+++ code-snippet-bot/tests/run-code-snippet/run-csharp-code-snippet-01.test.ts
@@ -5,11 +5,9 @@ test("run csharp code snippet 01", async (t) => {
   const { logged_content, execution_result } = await runCsharpCodeSample(`
     Console.WriteLine("Hello, World!");
   `)
-
-  // Assert that there's no error message in logged_content
-  t.falsy(logged_content.find((content) => content.startsWith("Error:")))
   
   // Assert that the execution result and log contain the expected output
   t.deepEqual(logged_content, ["Hello, World!"])
   t.is(execution_result, "Hello, World!\n")
-})
+});
+
