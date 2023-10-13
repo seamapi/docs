8761d7e1f95254d7581b9e175fe5af82b2c1b6e0
kainpets <pawelstepniak22@gmail.com>
2023-10-13 18:14:00 +0200

    expand capture group to include / of the endpoint

--- doc-snippet-bot/src/extract-endpoint-path.ts
+++ doc-snippet-bot/src/extract-endpoint-path.ts
@@ -1,6 +1,6 @@
 
 export default function extractEndpointPath(openapiSpec: string) {
-  const regex = /<!-- INJECT PROPERTIES (.+?) --!>/;
+  const regex = /<!-- INJECT PROPERTIES (\/.+?) --!>/;
   const match = openapiSpec.match(regex);
   return match && match[1] ? match[1] : '';
 }
--- doc-snippet-bot/tests/extract-endpoint-path.test.ts
+++ doc-snippet-bot/tests/extract-endpoint-path.test.ts
@@ -1,7 +1,7 @@
 import test from "ava"
 import extractEndpointPath from "../src/extract-endpoint-path"
 
-test('extractEndpointPath should correctly extract the endpoint path', (t) => {
+test('extractEndpointPath should correctly extract short endpoint path', (t) => {
   const openapiSpec = '<!-- INJECT PROPERTIES /users --!>';
   const endpointPath = extractEndpointPath(openapiSpec);
   const expectedEndpointPath = '/users';
@@ -13,5 +13,15 @@ test('extractEndpointPath should correctly extract longer endpoint paths', (t) =
   const endpointPath = extractEndpointPath(openapiSpec);
   const expectedEndpointPath = '/access_codes/create';
   t.is(endpointPath, expectedEndpointPath);
+});
+
+test('extractEndpointPath should return an error message when the endpoint doesn\'t begin with /', (t) => {
+  const validateEndpoint = (endpoint: string) => {
+    if (!endpoint.startsWith("/")) {
+      throw new Error("Endpoint must start with a / character.");
+    }
+  }
+  const error = t.throws(() => validateEndpoint("acces_code/create"));
+  t.is(error?.message, 'Endpoint must start with a / character.')
 });
 
