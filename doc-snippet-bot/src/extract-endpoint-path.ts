f846d9a043744eb6a753bfb101fad83d11d4181f
Pawel <41388251+kainpets@users.noreply.github.com>
2023-10-31 17:33:01 +0100

    Doc snippet bot (#204)
    * add doc-snippet-bot
    
    * doc snippet bot to inject properties into .md files
    
    * initial doc snippet bot root dir
    
    * Delete doc-snippet-bot/test.md
    
    * Update doc-snippet-bot/package.json
    
    * Update doc-snippet-bot/ava.config.js
    
    * initialize with typescript
    
    * change fetch to axios
    
    * update README
    
    * extract endpoint
    
    * expand capture group to include / of the endpoint
    
    * extract properties from endpoint
    
    * generate and format properties table from data
    
    * extract-properties-from-endpoint returns correct properties
    
    * refactor
    
    * reformat the generated table
    
    * make sure description is correctly fetched from OpenAPI
    
    * wip: test for generated table includes zod description
    
    * generate the table as two dimensional array, add extracted endpoint to generated table
    
    * properly format the table for table generation test
    
    * wip: doc-snippet-bot run from console
    
    * remove api-docs.sln
    
    * fix: test for finding files to inject properties into
    
    * remove DS_Store
    
    * remove another ds store
    
    * fix cli
    
    * some renames
    
    * minor renaming
    
    * cleanup types
    
    * prettier on everything
    
    ---------
    
    Co-authored-by: Severin Ibarluzea <seveibar@gmail.com>
    Co-authored-by: Severin Ibarluzea <seve700@gmail.com>

---
+++ doc-snippet-bot/src/extract-endpoint-path.ts
@@ -0,0 +1,5 @@
+export default function extractEndpointPath(openapiSpec: string) {
+  const regex = /<!-- INJECT PROPERTIES (\/.+?) --!>/
+  const match = openapiSpec.match(regex)
+  return match && match[1] ? match[1] : ""
+}
