55cf2d2528711ceededc7d735707b13a9d719145
Severin Ibarluzea <seveibar@gmail.com>
2023-10-31 09:13:28 -0700

    fix cli

--- doc-snippet-bot/src/cli.ts
+++ doc-snippet-bot/src/cli.ts
@@ -1,24 +1,21 @@
 #!/usr/bin/env node
 
-import yargs  from "yargs"
+import yargs from "yargs"
 import { hideBin } from "yargs/helpers"
-import chalk from "chalk"
 import injectPropertiesBot from "./inject-properties-bot"
 
 async function runDocSnippetBot() {
+  const chalk = (await import("chalk")).default
   yargs(hideBin(process.argv))
     .scriptName("doc-snippet-bot")
-    .command(
-      "doc-snippet-bot",
-      "run doc-snippet-bot",
-      {}, async () => {
-        await injectPropertiesBot()
-      })
-    .help().argv;
+    .command("doc-snippet-bot", "run doc-snippet-bot", {}, async () => {
+      await injectPropertiesBot()
+    })
+    .help().argv
 }
 
-runDocSnippetBot().catch((err) => {
+runDocSnippetBot().catch(async (err) => {
+  const chalk = (await import("chalk")).default
   console.error(chalk.red(err.message))
   process.exit(1)
 })
-
