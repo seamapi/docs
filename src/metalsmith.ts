e3f120360d5e90f8fd21af944872dac65f70e496
Evan Sosenko <evan@getseam.com>
2024-07-22 20:04:27 -0700

    Move output

--- src/metalsmith.ts
+++ src/metalsmith.ts
@@ -15,7 +15,7 @@ Metalsmith(rootDir).source('./docs/api').destination('../docs/sdk').clean(true)
 
 Metalsmith(rootDir)
   .source('./docs')
-  .destination('../docs')
+  .destination('../generate-docs')
   .clean(false)
   .use(
     metadata({
