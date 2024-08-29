3ac334ef2627528e29f7df21989913a8750ce7bb
Evan Sosenko <evan@getseam.com>
2024-08-29 22:19:53 +0000

    Format with ruff

--- src/lib/format-code.ts
+++ src/lib/format-code.ts
@@ -1,4 +1,6 @@
 import type { CodeSampleSyntax } from '@seamapi/blueprint'
+import commandExists from 'command-exists'
+import { execa } from 'execa'
 import { format as prettier } from 'prettier'
 
 export const formatCode = async (
@@ -7,9 +9,9 @@ export const formatCode = async (
 ): Promise<string> => {
   switch (syntax) {
     case 'javascript':
-      return await prettier(content, { parser: 'typescript' })
+      return await formatJavascript(content)
     case 'python':
-      return content
+      return await formatPython(content)
     case 'ruby':
       return content
     case 'php':
@@ -17,8 +19,28 @@ export const formatCode = async (
     case 'bash':
       return content
     case 'json':
-      return await prettier(content, { parser: 'json' })
+      return await formatJson(content)
     default:
       return content
   }
+}
+
+const formatJavascript = async (content: string): Promise<string> => {
+  return await prettier(content, { parser: 'typescript' })
+}
+
+const formatPython = async (content: string): Promise<string> => {
+  try {
+    await commandExists('ruff')
+    const result = await execa({ input: content })`ruff format -`
+    return result.stdout
+  } catch {
+    // eslint-disable-next-line no-console
+    console.warn('Skipping python formatting: ruff is not installed')
+    return content
+  }
+}
+
+const formatJson = async (content: string): Promise<string> => {
+  return await prettier(content, { parser: 'json' })
 }
