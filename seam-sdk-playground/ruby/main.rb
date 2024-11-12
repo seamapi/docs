6115aa95194653f8ee891561b490940af9dc8bee
DebbieAtSeam <145377258+DebbieAtSeam@users.noreply.github.com>
2024-11-12 11:31:30 -0500

    Update main.rb

--- seam-sdk-playground/ruby/main.rb
+++ seam-sdk-playground/ruby/main.rb
@@ -5,6 +5,6 @@ require 'seam'
 random_number = rand(1e6).to_i
 api_url = "https://pws#{random_number}#{random_number}.fakeseamconnect.seam.vc"
 
-seam = Seam.new(base_uri: api_url, api_key: "my-api-key")
+seam = Seam.new(base_uri: api_url, api_key: "your-api-key")
 
 print seam.devices.list
