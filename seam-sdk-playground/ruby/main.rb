b75e3257131e02d0286e5266867f82096b069d8f
DebbieAtSeam <145377258+DebbieAtSeam@users.noreply.github.com>
2024-11-14 21:11:27 -0500

    Switch playground to seam Ruby SDK (#418)
    * Switch to generated Ruby SDK
    
    * ci: Generate docs
    
    * Change Ruby from seamapi to seam
    
    * Update main.rb
    
    * Update seam-sdk-playground/ruby/main.rb
    
    Co-authored-by: Evan Sosenko <evan@getseam.com>
    
    * Update seam-sdk-playground/ruby/main.rb
    
    ---------
    
    Co-authored-by: Seam Bot <devops@getseam.com>
    Co-authored-by: Evan Sosenko <evan@getseam.com>

--- seam-sdk-playground/ruby/main.rb
+++ seam-sdk-playground/ruby/main.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-require 'seamapi'
+require 'seam'
 
 random_number = rand(1e6).to_i
 api_url = "https://pws#{random_number}#{random_number}.fakeseamconnect.seam.vc"
 
-client = Seam::Client.new(base_uri: api_url, api_key: 'seam_apikey1_token')
+seam = Seam.new(endpoint: api_url, api_key: "seam_apikey1_token")
 
-print client.devices.list
+print seam.devices.list
