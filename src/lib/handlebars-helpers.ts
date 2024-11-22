f910c041c355be62a0674389fe31142b1f7eab25
Andrii Balitskyi <10balian10@gmail.com>
2024-11-22 17:15:52 +0100

    Remove not helper

--- src/lib/handlebars-helpers.ts
+++ src/lib/handlebars-helpers.ts
@@ -15,10 +15,6 @@ export const add = (v1: number, v2: number): number => {
   return v1 + v2
 }
 
-export const not = (value: boolean): boolean => {
-  return !value
-}
-
 export const toNoCase = (str: string): string => {
   return noCase(str)
 }
