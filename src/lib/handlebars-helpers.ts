270a06d0b3a012aa071669e9a37d827b1065607d
Andrii Balitskyi <10balian10@gmail.com>
2024-11-22 18:08:30 +0100

    Update template, remove toNoCase helper

--- src/lib/handlebars-helpers.ts
+++ src/lib/handlebars-helpers.ts
@@ -1,4 +1,4 @@
-import { capitalCase, noCase } from 'change-case'
+import { capitalCase } from 'change-case'
 
 export const eq = (v1: unknown, v2: unknown): boolean => {
   return v1 === v2
@@ -13,10 +13,6 @@ export const or = (...args: unknown[]): boolean => {
 
 export const add = (v1: number, v2: number): number => {
   return v1 + v2
-}
-
-export const toNoCase = (str: string): string => {
-  return noCase(str)
 }
 
 export const toCapitalCase = (str: string): string => {
