6e09c5ef04fc3c36115c6486ec7549f38b07c704
Andrii Balitskyi <84702959+andrii-balitskyi@users.noreply.github.com>
2024-11-25 12:13:28 +0100

    Report untitled routes and endpoints (#432)
    Co-authored-by: Seam Bot <devops@getseam.com>

--- src/lib/report.ts
+++ src/lib/report.ts
@@ -10,6 +10,8 @@ import type {
 import { openapi } from '@seamapi/types/connect'
 import type Metalsmith from 'metalsmith'
 
+import { PathMetadataSchema } from './path-metadata.js'
+
 const defaultDeprecatedMessage = 'No deprecated message provided'
 const defaultDraftMessage = 'No draft message provided'
 const defaultUndocumentedMessage = 'No undocumented message provided'
@@ -21,6 +23,7 @@ interface Report {
   deprecated: ReportSection
   extraResponseKeys: MissingResponseKeyReport[]
   endpointsWithoutCodeSamples: string[]
+  noTitle: Pick<ReportSection, 'routes' | 'endpoints'>
 }
 
 interface ReportSection {
@@ -77,6 +80,10 @@ function generateReport(metadata: Metadata): Report {
     deprecated: createEmptyReportSection(),
     extraResponseKeys: [],
     endpointsWithoutCodeSamples: [],
+    noTitle: {
+      routes: [],
+      endpoints: [],
+    },
   }
 
   const resources = metadata.resources ?? {}
@@ -86,7 +93,7 @@ function generateReport(metadata: Metadata): Report {
 
   const routes = metadata.routes ?? []
   for (const route of routes) {
-    processRoute(route, report)
+    processRoute(route, report, metadata)
   }
 
   return report
@@ -171,7 +178,7 @@ function processProperty(
   }
 }
 
-function processRoute(route: Route, report: Report): void {
+function processRoute(route: Route, report: Report, metadata: Metadata): void {
   if (route.isUndocumented) {
     report.undocumented.routes.push({
       name: route.path,
@@ -191,6 +198,14 @@ function processRoute(route: Route, report: Report): void {
       name: route.path,
       reason: defaultDraftMessage, // TODO: draftMessage
     })
+  }
+
+  const pathMetadata =
+    'pathMetadata' in metadata
+      ? PathMetadataSchema.parse(metadata.pathMetadata)
+      : {}
+  if (pathMetadata[route.path]?.title == null && !route.isUndocumented) {
+    report.noTitle.routes.push({ name: route.path })
   }
 
   if (route.namespace != null) {
@@ -252,6 +267,10 @@ function processEndpoint(endpoint: Endpoint, report: Report): void {
 
   if (endpoint.codeSamples.length === 0) {
     report.endpointsWithoutCodeSamples.push(endpoint.path)
+  }
+
+  if (endpoint.title.length === 0 && !endpoint.isUndocumented) {
+    report.noTitle.endpoints.push({ name: endpoint.path })
   }
 
   processResponseKeys(endpoint, report)
